ad5a5d93b558a6e5f061141b2a9b94921871dcc9
nick <nickadaniya@gmail.com>
2022-09-27 11:27:17 -1000

    the rest

--- dynamic-array.js
+++ dynamic-array.js
@@ -11,6 +11,7 @@ class DynamicArray {
   }
 
   push(val) {
+    if (this.length + 1 > this.capacity) this.resize()
     this.data[this.length] = val;
     this.length++;
   }
@@ -18,19 +19,27 @@ class DynamicArray {
 
   pop() {
     let val = this.data[this.length - 1];
-    this.data[this.length -1] = undefined;
+    this.data[this.length - 1] = undefined;
     this.length--;
     return val;
   }
 
   shift() {
+    let val = this.data[0];
 
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
 
+    this.length--;
+    this.data[this.length] = undefined;
+    return val;
   }
 
   unshift(val) {
+    if (this.length + 1 > this.capacity) this.resize()
     this.length++;
-    for(let i = this.length; i > 0; i--) {
+    for (let i = this.length; i > 0; i--) {
       this.data[i] = this.data[i - 1];
     }
     this.data[0] = val;
@@ -38,13 +47,21 @@ class DynamicArray {
   }
 
   indexOf(val) {
-
-    // Your code here
+    for (let i = this.length - 1; i >= 0; i--) {
+      if (this.data[i] === val) return i
+    }
+    return -1
   }
 
   resize() {
+    const newCapacity = this.capacity * 2;
+    this.capacity = newCapacity;
+    const oldData = this.data;
+    this.data = new Array(this.capacity);
 
-    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = oldData[i];
+    }
   }
 
 }
